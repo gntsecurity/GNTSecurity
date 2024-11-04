06c5580e9de494ef94c18961a1081602be9ccf8b
devlizgate <gsmith@lizgate.com>
2024-11-04 09:06:55 -0500

    Nav update

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+<!-- JavaScript to Toggle Navbar on Mobile -->
+<script>
+    document.getElementById('menu-toggle').addEventListener('click', function() {
+        const navbar = document.querySelector('.navbar');
+        navbar.classList.toggle('active');
+    });
+</script>
